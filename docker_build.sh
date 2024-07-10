a9d45b1399561fcd8483e40cc4ce03c0725691b9
pangz <pangz.lab@gmail.com>
2024-07-11 03:01:58 +0900

    updated docker
    - added compose.yaml
    - updated docker_build.sh to add tags
    - updated nginx configuration

--- docker_build.sh
+++ docker_build.sh
@@ -18,5 +18,4 @@ echo "Creating build files ..."
 ./dist.sh
 
 echo "Building $IMAGE_OWNER/$IMAGE_NAME image with version: $VERSION"
-sudo docker build -t $IMAGE_OWNER/$IMAGE_NAME:$VERSION .
-#sudo docker run -d --env-file .env -p 2221:80 pangzlab/verus-explorer-ui 
+sudo docker build --no-cache -t $IMAGE_OWNER/$IMAGE_NAME:latest -t $IMAGE_OWNER/$IMAGE_NAME:$VERSION .
