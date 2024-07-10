f55217b465e9ba3fe70a2e1fadf922456e4bb4b5
pangz <pangz.lab@gmail.com>
2024-07-11 03:31:40 +0900

    relocated docker scripts to a separate folder
    - updated nginx config
    - updated docker_build.sh logic
    - updated jap translation for text input bar

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -13,9 +13,19 @@ fi
 VERSION=$1
 IMAGE_OWNER=pangzlab
 IMAGE_NAME=verus-explorer-ui
+BASE_FOLDER=docker
+
+echo "Removing old build files ..."
+rm -rf ./dist
 
 echo "Creating build files ..."
+cd ../
 ./dist.sh
+
+echo "Moving build files to $BASE_FOLDER ..."
+mv dist $BASE_FOLDER/
+
+cd $BASE_FOLDER
 
 echo "Building $IMAGE_OWNER/$IMAGE_NAME image with version: $VERSION"
 sudo docker build --no-cache -t $IMAGE_OWNER/$IMAGE_NAME:latest -t $IMAGE_OWNER/$IMAGE_NAME:$VERSION .
