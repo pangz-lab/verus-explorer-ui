ddf0d7492478646c5113070f676ba1dce123a912
pangz <pangz.lab@gmail.com>
2024-07-19 01:11:59 +0900

    replaced identity.name with .identity.friendlyname,

--- public/src/js/controllers/address.js
+++ public/src/js/controllers/address.js
@@ -80,7 +80,7 @@ angular
                 .then(function (addressResult) {
                     const r = addressResult.data;
                     $scope.currencyBalances.push({
-                        currency: r.identity.name,
+                        currency: r.identity.friendlyname,
                         balance: pair[kCurrency],
                     })
                 });
